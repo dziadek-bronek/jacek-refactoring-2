8e7fd14bb5724331544eea59b4561b605ded23a6
Jacek Skowron <skowron76@gmail.com>
2022-03-30 17:08:08 +0200

    Get rid dynamic cast. Still compilation error.

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -215,23 +215,17 @@ Controller::Segment Controller::getNewHead() const
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
-    } catch (std::bad_cast&) {
-        try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
-            try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
-            } catch (std::bad_cast&) {
-                try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
-                } catch (std::bad_cast&) {
-                    throw UnexpectedEventException();
-                }
-            }
-        }
-    }
+	if(e.getMessageId() == 0x20) {
+        handleTimePassed(EventT<TimeoutInd> const& (*e));
+    } else if(e.getMessageId() == 0x10) {
+            handleDirectionChange(EventT<DirectionInd> const& (*e));
+	} else if(e.getMessageId() == 0x40) {
+		handleFoodPositionChange(EventT<FoodInd> const& (*e));
+	} else if(e.getMessageId() == 0x42) {
+		handleNewFood(EventT<FoodResp> const& (*e));
+	} else (std::bad_cast&) {
+		throw UnexpectedEventException();
+	}
 }
 
 } // namespace Snake
